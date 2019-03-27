a2d21ed3b17c0faa0cc7229a0fe2e8e05e1da4ba
luoMingQ <luosanchun@foxmail.com>
2019-03-27 17:04:47 +0800

    convertUtil添加注释

--- src/main/java/com/mzkj/util/ConvertUtil.java
+++ src/main/java/com/mzkj/util/ConvertUtil.java
@@ -13,17 +13,36 @@ import com.github.pagehelper.PageHelper;
  */
 public class ConvertUtil {
 
-    //对象转json
+    /**
+     * 对象转json
+     * return
+     * Author luosc
+     * param
+     * Date 2019-03-27 17:02
+     */
     public static String obj2Json(Object obj) {
         String jsonStr = JSON.toJSONString(obj);
         return jsonStr;
     }
 
-    //json转对象
+    /**
+     * json转对象
+     * return
+     * Author luosc
+     * param
+     * Date 2019-03-27 17:02
+     */
     public static <T> T json2Obj(String json, Class<T> target) {
         return JSON.parseObject(json, target);
     }
 
+    /**
+     * 源对象obj 转target对象
+     * return
+     * Author luosc
+     * param
+     * Date 2019-03-27 17:02
+     */
     public static <T> T objectCopyParams(Object obj, Class<T> target) {
         String jsonStr = JSON.toJSONString(obj);
         return JSON.parseObject(jsonStr, target);
